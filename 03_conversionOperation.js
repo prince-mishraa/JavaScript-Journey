b12921430d31714d1ca4b8b3f7d2bfe4fb729362
Prince Mishra <princemishra8104@gmail.com>
2025-08-06 12:18:32 +0530

    Conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "prince"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" can be converted to number
+// "33abc" => NaN (Not a Number)
+// true => 1, false => 0
+
+// let isLoggedIn = " "
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// "" => false
+// " " => true
+// "prince" => true
+
+let somenum = 98
+let stringNumber = String(somenum)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let score = "prince"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
