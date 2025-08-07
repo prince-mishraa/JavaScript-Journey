afd6808fa6de791da9eb3c25c90d51746c9a78c2
Prince Mishra <princemishra8104@gmail.com>
2025-08-07 21:28:12 +0530

    stack and heap memory

--- datatypes.js
+++ datatypes.js
@@ -9,20 +9,41 @@
 
 // console.log(id === anotherId)
 
-const Bignumber = 54846618778548854155205535n
+// const Bignumber = 54846618778548854155205535n
 
 
 
-//  Reference (non-primitive) Data Types: Objects, Arrays, Functions 
+// //  Reference (non-primitive) Data Types: Objects, Arrays, Functions 
 
-const heros = ["shaktimaan", "doga", "krish"]
-let myobj = {
-    name: "Prince",
-    age: 23,
+// const heros = ["shaktimaan", "doga", "krish"]
+// let myobj = {
+//     name: "Prince",
+//     age: 23,
+// }
+
+// const myfuction = function() {
+//     console.log("Hii")
+// }
+
+// console.log(typeof myobj)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), heap(Non-Primitive)
+
+// let myname = "Prince"
+// let username = myname
+// username = "Princemishra"
+// console.log(myname);
+
+let user = {
+    email : "Iser@gmail.com",
+    user_name: "user123"
 }
-
-const myfuction = function() {
-    console.log("Hii")
+let user2 = {
+    email : "user@gmail.com",
+    user_name: "user2"
 }
-
-console.log(typeof myobj)
+user2 = user
+console.log(user)
+console.log(user2);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,12 @@
-const id = Symbol('234')
-// const anotherId = Symbol('234')
+let myname = "Prince"
+let username = myname
+username = "Princemishra"
+console.log(myname);
 
-// console.log(id === anotherId)
+let user = {
+    email : "Iser@gmail.com",
+    user_name: "user123"
+}
+let user2 = user
+
+console.log(user2);
